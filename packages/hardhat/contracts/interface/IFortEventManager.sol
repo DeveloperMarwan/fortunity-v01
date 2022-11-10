555e7b36ac66b3b13d96aa5ce83e690e43d007f7
Austin <pizzahi5@gmail.com>
2022-11-10 15:39:00 -0800

    Added_EventManager_for_Kevin

---
+++ packages/hardhat/contracts/interface/IFortEventManager.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.6;
+
+contract FortEventManager {
+    // Added to simplify data collection for Kevin, temporary
+    event IndexPriceUpdated(uint256);
+    event MarketPriceUpdated(uint256);
+    event FundingRateUpdated(uint256);
+    
+}
