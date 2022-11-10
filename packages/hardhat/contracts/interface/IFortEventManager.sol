d0d9ffdb33080eb3fadee5d18070681a35c95982
Austin <pizzahi5@gmail.com>
2022-11-10 15:53:02 -0800

    Added_event_calls

--- packages/hardhat/contracts/interface/IFortEventManager.sol
+++ packages/hardhat/contracts/interface/IFortEventManager.sol
@@ -1,10 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
+// @dev Will be replacing with Subgraph or Moralis for future data retrieval
+
 contract FortEventManager {
     // Added to simplify data collection for Kevin, temporary
-    event IndexPriceUpdated(uint256);
-    event MarketPriceUpdated(uint256);
-    event FundingRateUpdated(uint256);
+    event IndexPriceUpdated(uint256 _indexPrice);
+    event MarketPriceUpdated(uint256 _mktPrice);
+    event FundingRateUpdated(uint256 _fundingRate);
     
+    event IndexTwap(uint256 _indexTwap);
+    event TotalLiquidity(uint256 _poolLiquidity);
+
+    event ActivityChange(
+        Action _action, 
+        uint256 positionSize,
+        uint256 colleratalChange, 
+        uint256 executionPrice, 
+        uint256 tradeFee,
+        uint256 timestamp);
+
+    enum Action {
+        Buy,
+        Sell,
+        Stake,
+        Withdraw
+    }
 }
